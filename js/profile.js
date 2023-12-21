68ad85648c031e347a61d3e64dfcc07c9d199db1
andrewmikhno <w.mikhno@gmail.com>
2023-12-21 14:09:39 +0300

    add: lab-6

--- js/profile.js
+++ js/profile.js
@@ -1,60 +1,44 @@
-// profile.js
-
+//profile.js
 document.addEventListener('DOMContentLoaded', function () {
     var profileForm = document.getElementById('profileForm');
-    var resultsContainer = document.getElementById('resultsContainer');
     var successMessage = document.getElementById('successMessage');
+
+    var savedFormData = JSON.parse(localStorage.getItem('profileData')) || {};
+    restoreFormData(savedFormData);
 
     profileForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        var firstName = document.getElementById('firstName').value;
-        var lastName = document.getElementById('lastName').value;
-        var city = document.getElementById('city').value;
-        var address = document.getElementById('address').value;
-        var phoneNumber = document.getElementById('phoneNumber').value;
+        var formData = {
+            name: document.getElementById('firstName').value,
+            surname: document.getElementById('lastName').value,
+            email: document.getElementById('email').value,
+            address: document.getElementById('address').value,
+            city: document.getElementById('city').value,
+            phone: document.getElementById('phoneNumber').value,
+            website: document.getElementById('website').value,
+            company: document.getElementById('company').value,
+            companyCatchPhrase: document.getElementById('companyCatchPhrase').value
+        };
 
-        var resultsList = document.createElement('ul');
-        resultsList.classList.add('results-list');
-
-        appendResultItem(resultsList, 'Nombre:', `${firstName} ${lastName}`);
-        appendResultItem(resultsList, 'Ciudad:', city);
-        appendResultItem(resultsList, 'Dirección:', address);
-        appendResultItem(resultsList, 'Número de Teléfono:', phoneNumber);
-
-        resultsContainer.innerHTML = '';
-
-        resultsContainer.appendChild(resultsList);
+        saveToLocalStorage(formData);
 
         successMessage.style.display = 'block';
-
-        saveToLocalStorage({
-            firstName: firstName,
-            lastName: lastName,
-            city: city,
-            address: address,
-            phoneNumber: phoneNumber
-        });
     });
-
-    function appendResultItem(list, label, value) {
-        var listItem = document.createElement('li');
-        listItem.innerHTML = `<strong>${label}</strong> ${value}`;
-        list.appendChild(listItem);
-    }
 
     function saveToLocalStorage(data) {
         localStorage.setItem('profileData', JSON.stringify(data));
     }
 
-    var storedProfileData = localStorage.getItem('profileData');
-    if (storedProfileData) {
-        storedProfileData = JSON.parse(storedProfileData);
-
-        document.getElementById('firstName').value = storedProfileData.firstName;
-        document.getElementById('lastName').value = storedProfileData.lastName;
-        document.getElementById('city').value = storedProfileData.city;
-        document.getElementById('address').value = storedProfileData.address;
-        document.getElementById('phoneNumber').value = storedProfileData.phoneNumber;
+    function restoreFormData(data) {
+        document.getElementById('firstName').value = data.name || '';
+        document.getElementById('lastName').value = data.surname || '';
+        document.getElementById('email').value = data.email || '';
+        document.getElementById('address').value = data.address || '';
+        document.getElementById('city').value = data.city || '';
+        document.getElementById('phoneNumber').value = data.phone || '';
+        document.getElementById('website').value = data.website || '';
+        document.getElementById('company').value = data.company || '';
+        document.getElementById('companyCatchPhrase').value = data.companyCatchPhrase || '';
     }
 });
